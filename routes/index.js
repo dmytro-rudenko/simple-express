672cae42527abe39a4aa08b6baec4f8ff2e4c72f
Didi <ph.dmitry.rudenko@gmail.com>
2023-09-25 18:48:15 +0300

    make delay

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   const date = new Date() 
   console.log(date)
+  await delay(50)
   res.json({ date });
 });
 
